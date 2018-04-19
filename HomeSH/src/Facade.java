f542a21b317241c0323cb672d33e4e98b0799017
47034 <47034@CEUEKTIPM-012.ue.poznan>
2018-04-19 12:59:25 +0200

    commit

--- HomeSH/src/Facade.java
+++ HomeSH/src/Facade.java
@@ -1,4 +1,33 @@
 
-public class Facade {
+public class Facade implements Device {
+	public Tuner tuner;
+	public CdPlayer cd;
+	public DvdPlayer dvd;
+	public Projector projector;
+	public Amplifier amplifier;
+	public Lights light;
+public void off(){
+	tuner.off();
+	cd.off();
+	dvd.off();
+	projector.off();
+	amplifier.off();
+	light.off();
+}
+
+@Override
+public void on() {
+	tuner.on();
+	cd.on();
+	dvd.on();
+	projector.on();
+	amplifier.on();
+	light.on();
+	
+}
+
 
 }
+	
+
+
