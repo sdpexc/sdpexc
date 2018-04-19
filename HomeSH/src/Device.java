8f78325115d94085fd0a0768a054cc2b0f471bb7
47034 <47034@CEUEKTIPM-012.ue.poznan>
2018-04-19 12:27:26 +0200

    message

--- HomeSH/src/Device.java
+++ HomeSH/src/Device.java
@@ -1,4 +1,12 @@
 
+
 public interface Device {
 
+	public void on();
+	public void off();
+	public String toString();
+	
+		
 }
+
+
---
+++ HomeSH/src/Tuner.java
@@ -0,0 +1,25 @@
+
+public class Tuner implements Device {
+
+	@Override
+	public void on() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void off() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
+	public void setAm() {
+	}
+	public void setFm() {
+	}
+	public void setFrequency() {
+	}
+	
+}
